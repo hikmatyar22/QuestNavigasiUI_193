5c840b2ac72874d4795fdfbb1a3791908dda8b4f
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-07 10:15:37 +0700

    membuat horizontal dividwer untuk garis pemisah

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -50,7 +50,11 @@ fun FormIsian(
                 label = { Text(text = "nama_lengkap") },
                 onValueChange = {}
             )
-
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(width = 250.dp), thickness = Thickness, color = Color.Red
+            )
 
 
 

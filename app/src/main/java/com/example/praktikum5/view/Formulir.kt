2c2a84ddffb6e78ac39e10a55d6f910ced5ca6b2
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-07 10:14:13 +0700

    membuat scaffold untuk halaman atas isinya yang dimulai dari topbar

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -28,4 +28,15 @@ fun FormIsian(
     jenisK:List<String> = listOf("Laki-Laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.home), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ){
+    }
 }

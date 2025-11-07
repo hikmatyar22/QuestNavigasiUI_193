8e3d4dfd2c23f4f020b4926443a3bf0bf8fb099c
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-07 13:34:16 +0700

    membuat file navigasi dan membuat enum class navigasi untuk menyimpan nilai formulirku dan detail serta membuat fungsi dataapp untuk menjalankan fungsi navigasi

---
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -0,0 +1,26 @@
+package com.example.praktikum5
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum5.view.FormIsian
+import com.example.praktikum5.view.TampilData
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+
+}
+
